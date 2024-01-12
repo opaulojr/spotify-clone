ad4ff70cf389c2167f5a9e8282b2331f409abbb3
opaulojr <paulojr.hs@gmail.com>
2024-01-12 15:10:52 -0300

    fix-css-library

--- components/Library.tsx
+++ components/Library.tsx
@@ -10,26 +10,30 @@ function Library() {
 
   return (
     <div className="flex flex-col">
-      <div className="
-      flex
-      items-center
-      justify-between
-      px-5
-      pt-4
-      "
-      >
-        <div className="
-        inline-flex
+      <div
+        className="
+        flex
         items-center
-        gap-x-2
+        justify-between
+        px-5
+        pt-4
         "
+      >
+        <div
+          className="
+          inline-flex
+          items-center
+          gap-x-2
+          "
         >
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="
-          text-neutral-400
-          font-medium
-          text-md
-          "
+
+          <p
+            className="
+            text-neutral-400
+            font-medium
+            text-md
+            "
           >
             Your Library
           </p>
