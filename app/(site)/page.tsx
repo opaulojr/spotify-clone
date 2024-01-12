85b4499912c9205554b6431b355d9ffd338f6eca
opaulojr <paulojr.hs@gmail.com>
2024-01-12 14:44:55 -0300

    implement-liked-songs-button

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from '@/components/Header';
+import ListButton from '@/components/ListButton';
 
 export default function Home() {
   return (
@@ -13,8 +14,40 @@ export default function Home() {
       "
     >
       <Header>
-        Header Description
+        <div>
+          <h1 className="text-white text-3xl font-semibold">
+            Welcome back
+          </h1>
+
+          <div
+            className="
+            grid
+            grid-cols-1
+            sm:grid-cols-2
+            xl:grid-cols-3
+            2xl:grid-cols-4
+            gap-3
+            mt-4
+            "
+          >
+            <ListButton
+              image="/images/liked.png"
+              name="Liked Songs"
+              href="liked"
+            />
+          </div>
+        </div>
       </Header>
+
+      <div className="mt-2 mb-7 px-6">
+        <h1 className="text-white text-2xl font-semibold">
+          Newest Songs
+        </h1>
+
+        <div>
+          List of songs
+        </div>
+      </div>
     </div>
   );
 }
